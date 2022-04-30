ac20d7827ce21a91c2ebd7212f698a03e8962ebc
oss-m <ossama.elayat@gmail.com>
2022-05-01 01:21:07 +0200

    Add project files.

---
+++ Universties/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Universties
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("         Welcome to Universties Application");
+            Console.WriteLine("Please Enter Universty Name or Done if Finished");
+            List<Universty> universties = new List<Universty>();
+            string uni_input = Console.ReadLine();
+            while (uni_input != "Done")
+            {
+                var uni = new Universty();
+                uni.UniverstyName = uni_input;
+                universties.Add(uni);
+                Console.WriteLine("Please Enter Next Universty Name or Done if Finished");
+                uni_input = Console.ReadLine();
+            }
+            Console.WriteLine("The List of Universties are:");
+            for (int i = 0; i < universties.Count; i++)
+            {
+                Console.WriteLine("{0}. {1}",i+1,universties[i].UniverstyName);
+            }
+
+
+        }
+    }
+}
---
+++ Universties/Universty.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Universties
+{
+    public class Universty
+    {
+        public string UniverstyName { get; set; }
+        public List<Colledge> Colledges { get; set; }
+        public List<Student> Students { get; set; }
+        public Universty()
+        {
+            Colledges = new List<Colledge>();
+            Students = new List<Student>();
+        }
+    }
+}
