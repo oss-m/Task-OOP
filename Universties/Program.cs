2665bb050034e60ae6ee68573514ba82bf9f612b
oss-m <ossama.elayat@gmail.com>
2022-05-14 00:42:33 +0200

    ty

--- Universties/Program.cs
+++ Universties/Program.cs
@@ -10,12 +10,10 @@ namespace Universties
             //Universties Input
             Console.WriteLine("Entering Universties Names");
             var uni = new Universty();
-            uni.Universties = new List<Operations<Universty>>();
             uni.Universties.AddRange(uni.Create(uni.Universties));
             //Colledges Input
             foreach (var item_uni in uni.Universties)
             {
-                item_uni.Colledges = new List<Operations<Colledge>>();
                 Console.WriteLine("Entering Colledges Names for University {0}", item_uni.Name);
                 var coll = new Colledge();
                 item_uni.Colledges.AddRange(coll.Create(item_uni.Colledges));
@@ -26,7 +24,6 @@ namespace Universties
             {
                 foreach (var item_coll in item_uni.Colledges)
                 {
-                    item_coll.Departments = new List<Operations<Department>>();
                     Console.WriteLine("Entering Departments Names for\nColledge {0} of {1} University", item_coll.Name, item_uni.Name);
                     var dep = new Department();
                     item_coll.Departments.AddRange(dep.Create(item_coll.Departments));
@@ -39,7 +36,6 @@ namespace Universties
                 {
                     foreach (var item_dep in item_coll.Departments)
                     {
-                        item_dep.Subjects = new List<Operations<Subject>>();
                         Console.WriteLine("Entering Subjects Names for\nDepartment {0} of Colledge {1} of University {2}", item_dep.Name, item_coll.Name, item_uni.Name);
                         var sub = new Subject();
                         item_dep.Subjects.AddRange(sub.Create(item_dep.Subjects));
@@ -55,7 +51,6 @@ namespace Universties
                     {
                         foreach (var item_sub in item_dep.Subjects)
                         {
-                            item_sub.Staffs = new List<Operations<Staff>>();
                             Console.WriteLine("Entering Staffs Names for\nSubject {0} of Department {1} of Colledge {2} of University {3}", item_sub.Name, item_dep.Name, item_coll.Name, item_uni.Name);
                             var staff = new Staff();
                             item_sub.Staffs.AddRange(staff.Create(item_sub.Staffs));
@@ -70,7 +65,6 @@ namespace Universties
                 {
                     foreach (var item_dep in item_coll.Departments)
                     {
-                        item_dep.Students = new List<Operations<Student>>();
                         Console.WriteLine("Entering Students Names for\nDepartment {0} of Colledge {1} of University {2}", item_dep.Name, item_coll.Name, item_uni.Name);
                         var stu = new Student();
                         item_dep.Students.AddRange(stu.Create(item_dep.Students));
